131d877ca35c6118526cc8546447ac65643ecacf
Anna Techkina <an.techkina@mail.ru>
2023-03-18 16:23:30 +0400

    add Task2_2

---
+++ C#/Lesson_2/Task_2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int rez = 0;
+int rez1 = 0;
+
+if (Num < 100)
+{
+  Console.WriteLine("третьей цифры нет");
+}
+else if (Num < 1000)
+{
+  rez1 = Num % 10;
+  Console.WriteLine(rez1);
+}
+else
+{
+  while (Num > 1000)
+  {
+    Num = Num / 10;
+    rez = Num % 10;
+  }
+  Console.WriteLine(rez);
+}
+
