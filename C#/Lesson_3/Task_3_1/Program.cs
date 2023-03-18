0f2482f119f6c633c69da0847149a32d21d3e041
Anna Techkina <an.techkina@mail.ru>
2023-03-19 00:55:09 +0400

    add Task3_1

--- C#/Lesson_3/Task_3_1/Program.cs
+++ C#/Lesson_3/Task_3_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Palindrom(int number)
+{
+    if(number >= 10000)
+    {
+        int first_number1 = number / 10000;
+        int last_number1 = number % 10;
+ 
+            if(first_number1 == last_number1)
+            {
+                number = number / 10;
+                int first_number2 = (number / 100)%10;
+                int last_number2 = number % 10;
+                if(first_number2 == last_number2)
+                    Console.WriteLine("Число является палиндром");
+            }
+            else 
+            Console.WriteLine("Число не является палиндромом"); 
+    }
+    else
+    Console.WriteLine("Нужно ввести пятизначное число");
+}
+ 
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+Palindrom(number);
