7da2137af518a2e9dda645b55da6977d6fe76795
Anna Techkina <an.techkina@mail.ru>
2023-03-18 16:18:22 +0400

    add Lesson_2

---
+++ C#/Lesson_2/Task_2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int rez = 0;
+
+if (Num < 10)
+  Console.WriteLine("нет");
+else
+    while (Num > 100) 
+      Num = Num / 10;
+    rez = Num % 10;
+    
+Console.WriteLine(rez);
