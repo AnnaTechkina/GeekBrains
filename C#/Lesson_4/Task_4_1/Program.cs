e3b2667ec0561e874dc23507b93dadc37ec78ed5
Anna Techkina <an.techkina@mail.ru>
2023-03-21 11:25:39 +0400

    add Lesson4

---
+++ C#/Lesson_4/Task_4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int rezalt = 1;
+
+Console.WriteLine(Degree(a,b));
+void Degree(int a, int b)
+{
+while (i < b)
+{
+  rezalt*=a;
+  i++;
+  return rezalt;
+}
+}
+//Console.WriteLine(rezalt);
