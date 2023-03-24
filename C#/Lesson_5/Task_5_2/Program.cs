d10f680f5457ff6566c915062ad6e0fedc35d132
Anna Techkina <an.techkina@mail.ru>
2023-03-24 21:19:01 +0400

    add Task2

---
+++ C#/Lesson_5/Task_5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] array = new int [8];
+int sum = 0;
+RandomNumbers(array);
+PrintArray(array);
+SumArray(array);
+
+void RandomNumbers(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(-20,20);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  foreach(int item in array)
+  {
+    Console.Write($"{item}" + " ");
+  }
+  Console.WriteLine(" ");
+}
+
+void SumArray(int[] array)
+{
+  for (int j = 0; j < array.Length; j++)
+  {
+    if (j % 2 ==0) sum += array[j]; 
+  }
+  Console.WriteLine(sum);
+}
