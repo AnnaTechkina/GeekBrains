89fbab785471e71019fd1f845cfb8a6cfe1f9da0
Anna Techkina <an.techkina@mail.ru>
2023-03-28 14:12:41 +0400

    add Task1

--- C#/Lesson_6/Task_6_1/Program.cs
+++ C#/Lesson_6/Task_6_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//5 //0, 7, 8, -2, -2 -> 2
+//5 //1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество цифр: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[]array = new int [m];
+int count = 0;
+
+FillingArray(array);
+PrintArray(array);
+Console.WriteLine(ResultsArray(array));
+
+
+int ResultsArray(int []array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count+=1;
+    }       return count;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array) Console.Write(item + " ");
+}
+Console.WriteLine();
+
+void FillingArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-100, 300);
+}
+
+
+
