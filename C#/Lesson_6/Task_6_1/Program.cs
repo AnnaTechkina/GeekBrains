c696ca179e34d7174c6bedf7f899b44afbcd6d50
Anna Techkina <an.techkina@mail.ru>
2023-04-02 00:34:55 +0400

    add Task1,2,3

--- C#/Lesson_6/Task_6_1/Program.cs
+++ C#/Lesson_6/Task_6_1/Program.cs
@@ -2,22 +2,28 @@
 //5 //0, 7, 8, -2, -2 -> 2
 //5 //1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите количество цифр: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[]array = new int [m];
-int count = 0;
-
-FillingArray(array);
+int numbers = Prompt("Введите количество элементов: ");
+int[] array;
+array = FillingArray(numbers);
 PrintArray(array);
-Console.WriteLine(ResultsArray(array));
+Console.WriteLine($"Количество элементов больше 0 - {ResultsArray(array)}");
 
-
-int ResultsArray(int []array)
+int Prompt(string message)
 {
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] FillingArray(int numbers)
+{
+    int[] array = new int[numbers];
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] > 0) count+=1;
-    }       return count;
+        array[i] = Prompt($"Введите {i + 1} - й элемент");    
+    }
+    return array; 
 }
 
 void PrintArray(int[] array)
@@ -26,10 +32,21 @@ void PrintArray(int[] array)
 }
 Console.WriteLine();
 
-void FillingArray(int[] array)
+int ResultsArray(int []array)
 {
-    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-100, 300);
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count+=1;
+    }       
+    return count;
 }
+
+
+
+
+
+
 
 
 
---
+++ C#/Lesson_6/Task_6_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// //заданных уравнениями y1 = k1 * x + b1, y2 = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите четыре координаты");
+Console.Write("Координата b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double[] result = IntersectionPoint(b1, k1, b2, k2);
+
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.WriteLine("Прямые совпадают");
+} 
+else if (k1==k2)
+{
+    Console.WriteLine("Прямые параллельны"); 
+}
+else 
+{
+PrintIntersectionPoint(IntersectionPoint(b1, k1, b2, k2));
+}
+double [] IntersectionPoint(int b1, int k1, int b2, int k2)
+{
+    double left = k1 + (k2*(-1));
+    double right = b2 + (b1*(-1));
+    double x_0 = right / left;
+    double y_0 = k1 * x_0 + b1;
+    return new double [] {(x_0), (y_0)};
+}  
+
+void PrintIntersectionPoint(double [] result)
+{
+    Console.Write("Координаты пересечения: ");
+    foreach (double item in result)
+        Console.Write(item +" ");
+}
