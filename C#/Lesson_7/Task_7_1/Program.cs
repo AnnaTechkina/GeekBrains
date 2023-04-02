e4f4e29c8a20fb5c97a07a3e8a791c29fbb37d2e
Anna Techkina <an.techkina@mail.ru>
2023-04-02 23:11:58 +0400

    add Task1,2

---
+++ C#/Lesson_7/Task_7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+void FillingArray(int[,]array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+      for (int j = 0; j < array.GetLength(1); j++)
+        array[i,j] = new Random().Next(-50,50);
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i,j],3}\t");
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows,cols];
+FillingArray(array);
+PrintArray(array);
---
+++ C#/Lesson_7/Task_7_2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+//и возвращает позицию этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:1 4 7 2
+                      // 5 9 2 3
+                      // 8 4 2 4   17 -> такого числа в массиве нет;
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows,cols];
+int count = 0;
+Console.WriteLine("Введите искомое число: ");
+int find = Convert.ToInt32(Console.ReadLine());
+
+FillingArray(array);
+PrintArray(array);
+NumberSearch(array,find);
+
+void FillingArray(int[,]array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+      for (int j = 0; j < array.GetLength(1); j++)
+        array[i,j] = new Random().Next(-10,10);
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i,j],3}\t");
+    Console.WriteLine();
+  }
+}
+
+void NumberSearch(int[,] array, int find)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        if (array[i,j] == find)
+        {
+          count++;
+Console.Write($"позиция элемента {find} [{i},{j}]"+" ");    
+        }
+      } 
+    } 
+    if (count > 0) Console.WriteLine($"количество вхождений -{count}"); 
+    else Console.WriteLine("Такого элемента нет");    
+}
