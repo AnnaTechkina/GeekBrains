e2e0db0e85dc87cac176d614e5a37a722b3fa199
Anna Techkina <an.techkina@mail.ru>
2023-04-11 10:38:04 +0400

    add Lesson9

---
+++ C#/Lesson_9/Task_9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+while (n > 0)
+{
+    Console.WriteLine(n);
+    n--;
+}
