0ec957a25bcf7647b25e5f2ef9b7a9ce2e20722a
Анна <an.techkina@yandex.ru>
2023-07-20 14:29:53 +0400

    add task3

---
+++ JAVA/Lesson1/task3.java
@@ -0,0 +1,36 @@
+package JAVA.Lesson1;
+
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(
+                "Введите первое и второе число - ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(
+                "Выберете и введите тип операции, которую вы хотите выполнить (+, -, *, /, %) -");
+        char op = sc.next().charAt(0);
+        solve(a, b, op);
+    }
+
+    public static int solve(int a, int b, char op) {
+        int ans = 0;
+        if (op == '+') {
+            ans = a + b;
+        } else if (op == '-') {
+            ans = a - b;
+        } else if (op == '*') {
+            ans = a * b;
+        } else if (op == '/') {
+            ans = a / b;
+        } else if (op == '%') {
+            ans = a % b;
+        }
+        System.out.println("Your answer is - " + ans);
+        return ans;
+    }
+}
